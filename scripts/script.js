15988a3abf3fcec61c68c8fefd3c86d452a27d12
NancyBolstad <litchacha@gmail.com>
2019-05-24 21:12:10 +0200

    Added mobile nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,12 @@ navBarToggle.addEventListener('click', function() {
   const isHidden = document.getElementById('js-menu').style.display;
 
   if (isHidden === 'none') {
+    document.getElementById('js-navmenu-toggle').classList.remove('fa-bars');
+    document.getElementById('js-navmenu-toggle').classList.add('fa-times');
     document.getElementById('js-menu').style.display = 'block';
   } else {
+    document.getElementById('js-navmenu-toggle').classList.remove('fa-times');
+    document.getElementById('js-navmenu-toggle').classList.add('fa-bars');
     document.getElementById('js-menu').style.display = 'none';
   }
-  console.log('test');
 });
-
-console.log('hello');
