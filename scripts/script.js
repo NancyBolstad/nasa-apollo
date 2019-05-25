62dc35acf8050c1491a5c6d5f5e7af567ac30a5a
NancyBolstad <litchacha@gmail.com>
2019-05-25 10:50:23 +0200

    Added search overlay

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,25 @@ navBarToggle.addEventListener('click', function() {
   }
   mainNav.classList.toggle('active');
 });
+
+document.addEventListener(
+  'click',
+  function(event) {
+    if (event.target.matches('.js-navsearch-toggle')) {
+      openSearch();
+    }
+
+    if (event.target.matches('.btn-close-search')) {
+      closeSearch();
+    }
+  },
+  false
+);
+
+function openSearch() {
+  document.getElementById('js-search-overlay').style.display = 'block';
+}
+
+function closeSearch() {
+  document.getElementById('js-search-overlay').style.display = 'none';
+}
