2b5a3d44c485076ab20181a48b0e47968326496b
NancyBolstad <litchacha@gmail.com>
2019-06-08 14:16:49 +0200

    changed script js

--- scripts/script.js
+++ scripts/script.js
@@ -22,18 +22,6 @@ navBarToggle.addEventListener('click', function() {
   mainNav.classList.toggle('active');
 });
 
-navBarToggle.addEventListener('click', function() {
-  isMobileMenuHidden = !isMobileMenuHidden;
-  if (!isMobileMenuHidden) {
-    navBarToggle.classList.remove('fa-bars');
-    navBarToggle.classList.add('fa-times');
-  } else {
-    navBarToggle.classList.remove('fa-times');
-    navBarToggle.classList.add('fa-bars');
-  }
-  mainNav.classList.toggle('active');
-});
-
 document.addEventListener(
   'click',
   function(event) {
@@ -55,28 +43,3 @@ function openSearch() {
 function closeSearch() {
   document.getElementById('js-search-overlay').style.display = 'none';
 }
-
-
-
-function addElement () { 
-//create random number for image generation
-var unsplashNum = Math.floor(Math.random() * 1084) + 1;
-
-var newsContainer = document.getElementById("news-grid");
-// create a new article element 
-var newDiv = document.createElement("article");
-newDiv.className = "block";
-// and give it some content
-newDiv.innerHTML ='<div class="block-hero"><img src="https://unsplash.it/600/400?image='+ unsplashNum +'"></div><div class="block-body"><h2>' + fakeTitle[randomTitle()] + '</h2></div>';  
-// add the newly created element and its content into the DOM  
-//document.getElementById("news-grid").appendChild(newDiv);
-newsContainer.insertBefore(newDiv, newsContainer.childNodes[0]);
-
-}
-
-function removeElement(){
-var parent = document.getElementById("news-grid");
-var child = document.getElementsByTagName("article")[0];
-parent.removeChild(child);
-}
-
